a7b06862eb5153593d9c76cc585a4ad83f67c946
MDSandovalGoya <sandovalmatthew003@gmail.com>
2025-11-13 00:36:52 -0500

    semi final version(might be updated)

--- classes.js
+++ classes.js
@@ -1,6 +1,6 @@
 
-
 // Ball Class
+
 class Ball {
   constructor(x, y, col) {
     this.pos = createVector(x, y);
@@ -16,18 +16,21 @@ class Ball {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
-    this.vel.mult(0.995);
+    this.vel.mult(0.985); 
   }
 
   checkBar(bar) {
     let dx = this.pos.x - bar.x;
-    if (abs(dx) <= bar.len / 2) {
-      let yAtX = bar.y + sin(bar.angle) * dx;
-      if (this.pos.y + this.r / 2 > yAtX) {
-        this.pos.y = yAtX - this.r / 2;
-        this.vel.y *= -0.6;
-        this.vel.x += sin(bar.angle) * 3;
-      }
+    if (abs(dx) > bar.len / 2) return;
+
+    let yAtX = bar.y + sin(bar.angle) * dx;
+    let bottom = this.pos.y + this.r / 2;
+    let distance = bottom - yAtX;
+
+    if (this.vel.y > 0 && distance > -5 && distance < 12) {
+      this.pos.y = yAtX - this.r / 2;
+      this.vel.y *= -0.4;
+      this.vel.x += sin(bar.angle) * 0.8 + random(-0.2, 0.2);
     }
   }
 
@@ -40,8 +43,8 @@ class Ball {
   }
 }
 
-
 // Bar Class
+
 class Bar {
   constructor(x, y, len) {
     this.x = x;
@@ -66,8 +69,8 @@ class Bar {
   }
 }
 
+// Wave Class
 
-// Wave Class 
 class Wave {
   constructor(col) {
     this.col = col;
@@ -83,33 +86,29 @@ class Wave {
 
   show() {
     noFill();
-    // RGB colors 
     stroke(red(this.col), green(this.col), blue(this.col), this.alpha);
     strokeWeight(3);
-    // Draws the bottom arc
     arc(width / 2, height, this.radius * 2, this.radius * 2, PI, TWO_PI);
   }
 
   finished() { return this.alpha <= 0; }
 }
 
-
 // Rainbow Fill Wave Class
+
 class RainbowFillWave {
   constructor() {
-    this.t = 0;            
-    this.fade = false;     
-    this.fadeProgress = 0; 
-    this.alpha = 1.0; 
-    this.maxRadius = sqrt(sq(width / 2) + sq(height)); 
-    this.waveSpeed = 0.05; 
-    this.arcCount = 120;   
+    this.t = 0;
+    this.fade = false;
+    this.fadeProgress = 0;
+    this.alpha = 1.0;
+    this.maxRadius = sqrt(sq(width / 2) + sq(height));
+    this.waveSpeed = 0.05;
+    this.arcCount = 120;
   }
 
-  // Fade
-  update() { 
-    this.t += 0.001 + colorShift * 0.0003; 
-
+  update() {
+    this.t += 0.001 + colorShift * 0.0003;
     if (this.fade) {
       this.fadeProgress = min(this.fadeProgress + 0.012, 1);
       this.alpha = 1.0 * (1 - pow(this.fadeProgress, 2.3));
@@ -118,41 +117,82 @@ class RainbowFillWave {
 
   show() {
     push();
-    // hsb mode to make sure the color fill actually works for the final wave 
     colorMode(HSB, 360, 100, 100, 1);
     noFill();
     strokeWeight(1.5);
-
-    // for loop for the fill
     for (let i = 0; i < this.arcCount; i++) {
       let inter = i / this.arcCount;
-
-      // Calculate radius: 
-      let radiusOffset = (this.t * this.waveSpeed) % 2; 
-      let r = this.maxRadius * (inter + radiusOffset - 1); 
-
-      // Calculate hue:
+      let radiusOffset = (this.t * this.waveSpeed) % 2;
+      let r = this.maxRadius * (inter + radiusOffset - 1);
       let hue = (r / this.maxRadius) * 360 + (this.t * 3);
-      hue = (hue + colorShift * 50) % 360; 
+      hue = (hue + colorShift * 50) % 360;
       if (hue < 0) hue += 360;
-
-      // Calculate opacity:
-      let opacity = 1.0;
-      let posFromCenter = abs(inter - 0.5) * 2; 
-      opacity = pow(1 - posFromCenter, 2.5); 
-
+      let posFromCenter = abs(inter - 0.5) * 2;
+      let opacity = pow(1 - posFromCenter, 2.5);
       let c = color(hue, 80, 90, opacity * this.alpha);
       stroke(c);
-
       arc(width / 2, height, r * 2, r * 2, PI, TWO_PI);
     }
-    pop(); //restores 
+    pop();
   }
 
   startFadeOut() { this.fade = true; }
+  isDone() { return this.fade && this.alpha <= 0.005; }
+}
 
-  isDone() {
-    // makes sure that it actually Sends
-    return this.fade && this.alpha <= 0.005; 
+// Flipper Class
+
+class Flipper {
+  constructor(x, y, len, side) {
+    this.x = x;
+    this.y = y;
+    this.len = len;
+    this.side = side; 
+    this.angle = side === "left" ? -PI / 6 : PI / 6;
+    this.restAngle = this.angle;
+    this.range = PI / 5;
+  }
+
+  update(ballCount, counter = false) {
+    let speed = ballCount >= 3 ? 0.12 : 0.06;
+    let flap = sin(frameCount * speed) * this.range;
+    if (counter) flap *= -1;
+    this.angle = this.restAngle + flap;
+  }
+
+  checkHit(ball) {
+    let halfLen = this.len / 2;
+    let x1 = this.x - cos(this.angle) * halfLen;
+    let y1 = this.y - sin(this.angle) * halfLen;
+    let x2 = this.x + cos(this.angle) * halfLen;
+    let y2 = this.y + sin(this.angle) * halfLen;
+
+    let t = constrain(
+      ((ball.pos.x - x1) * (x2 - x1) + (ball.pos.y - y1) * (y2 - y1)) /
+        ((x2 - x1) ** 2 + (y2 - y1) ** 2),
+      0,
+      1
+    );
+    let closestX = x1 + t * (x2 - x1);
+    let closestY = y1 + t * (y2 - y1);
+    let d = dist(ball.pos.x, ball.pos.y, closestX, closestY);
+
+    if (d < ball.r / 2 + 6 && ball.pos.y > this.y - 40) {
+      let normal = createVector(ball.pos.x - this.x, ball.pos.y - this.y).normalize();
+      let bounceForce = p5.Vector.mult(normal, 4);
+      bounceForce.y = -abs(bounceForce.y) * 1.2;
+      ball.vel.add(bounceForce);
+      ball.pos.y -= 5;
+    }
+  }
+
+  show() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    stroke(255);
+    strokeWeight(10);
+    line(-this.len / 2, 0, this.len / 2, 0);
+    pop();
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
-//This is my midterm for the adjective fretful. I made this off the idea that you might be stressed doing an action or doing work, but if you finish, you will be satisfied
-//The game essentially represents this because by dropping the balls off the plank as fast as possible, you will see a rainbow. Fretfulness often feels that way
-// some updates that I will do: separate this into a class file and a game file, figure out how I can do this even more fretful and make the code itself less complicated
+// this is my somwhat final version of my midterm for my adjective, fretful. what I changed so far is separate my classes and objects 
+// so I can import it and edit it easier. I also added some moving bars so I can actually make this live up the word fretful
+// I know you asked for refrences so I'll put some video links that got me to most of this because I had a lot of help from youtube
+// I also got help from threads and documentation from the books, and the p5js website
+// https://www.youtube.com/watch?v=dJNFPv9Mj-Y
+// https://www.youtube.com/watch?v=4aoQcEiDYjk
+// https://www.youtube.com/results?search_query=p5js+collision+detection 
+// https://www.youtube.com/watch?v=gUTeWOfwECc 
+// the rainbow effect was something I semi figured out but it was from the documentation and I feel really proud of it so please take a look(its the same I am just putting emphasis on it).
+
 
 let balls = [];
 let waves = [];
@@ -14,90 +21,97 @@ let rainbowDuration = 12000;
 let colorShift = 0;
 let dragging = false;
 
-// distinct colors
-let ballColors = [
-  "#ff4d4d", "#4d94ff", "#4dff4d",
-  "#ffb84d", "#ff4dff", "#4dffff"
-];
+// Flippers
+let leftFlipper, rightFlipper;
+let topLeftFlipper, topRightFlipper;
+let topFlippersActive = false;
+
+// Colors
+let ballColors = ["#ff4d4d","#4d94ff","#4dff4d","#ffb84d","#ff4dff","#4dffff"];
 
 function setup() {
   createCanvas(900, 600);
   gravity = createVector(0, 0.3);
-  bar = new Bar(width / 2, height / 2, 360);
+  bar = new Bar(width/2, height/2, 360);
   spawnBalls(1);
+
+  // bottom flippers
+  let bottomY = height - 70;
+  leftFlipper = new Flipper(140, bottomY, 160, "left");
+  rightFlipper = new Flipper(width-140, bottomY, 160, "right");
 }
 
 function draw() {
   background(18);
 
-  // Draw bar if not in rainbow phase
+  // main bar & flippers
   if (gamePhase !== "rainbow") {
-    bar.update();
-    bar.show();
+    bar.update(); bar.show();
     handleCollisions();
+
+    leftFlipper.update(balls.length); rightFlipper.update(balls.length);
+    leftFlipper.show(); rightFlipper.show();
+
+    if (topFlippersActive || balls.length >= 3) {
+      if (!topFlippersActive) activateTopFlippers();
+      topLeftFlipper.update(balls.length, true); topRightFlipper.update(balls.length, true);
+      topLeftFlipper.show(); topRightFlipper.show();
+    }
   }
 
-  // Update and draw balls
-  for (let i = balls.length - 1; i >= 0; i--) {
+  // balls
+  for (let i = balls.length-1; i >= 0; i--) {
     const b = balls[i];
-    b.applyForce(gravity);
-    b.update();
-    if (gamePhase !== "rainbow") b.checkBar(bar);
+    b.applyForce(gravity); b.update();
+
+    // bounce walls
+    if (b.pos.x - b.r/2 < 0) { b.pos.x = b.r/2; b.vel.x*=-0.9; }
+    else if (b.pos.x + b.r/2 > width) { b.pos.x = width - b.r/2; b.vel.x*=-0.9; }
+
+    if (gamePhase !== "rainbow") {
+      b.checkBar(bar);
+      leftFlipper.checkHit(b); rightFlipper.checkHit(b);
+      if (topFlippersActive) { topLeftFlipper.checkHit(b); topRightFlipper.checkHit(b); }
+    }
+
     b.show();
 
-    // Remove ball if it falls below screen
-    if (b.pos.y - b.r > height + 50) {
+    // remove off-screen balls
+    if (b.pos.y - b.r > height+50) {
       lastDropTimes.push(millis());
       waves.push(new Wave(b.color));
-      balls.splice(i, 1);
+      balls.splice(i,1);
     }
   }
 
-  // Handle waves
+  // waves
   if (gamePhase !== "rainbow") {
-    for (let i = waves.length - 1; i >= 0; i--) {
-      const w = waves[i];
-      w.update();
-      w.show();
-      if (w.finished()) waves.splice(i, 1);
+    for (let i = waves.length-1; i>=0; i--) {
+      const w = waves[i]; w.update(); w.show();
+      if (w.finished()) waves.splice(i,1);
     }
-
-    if (balls.length === 0 && waves.length === 0) nextPhase();
+    if (balls.length===0 && waves.length===0) nextPhase();
   }
 
-  // Rainbow phase
+  // rainbow mode
   if (gamePhase === "rainbow") {
     if (!rainbowWave) rainbowWave = new RainbowFillWave();
+    rainbowWave.update(); rainbowWave.show();
 
-    rainbowWave.update();
-    rainbowWave.show();
-
-    if (millis() - rainbowStartTime > rainbowDuration) {
-      rainbowWave.startFadeOut();
-    }
-
+    if (millis() - rainbowStartTime > rainbowDuration) rainbowWave.startFadeOut();
     if (rainbowWave.isDone()) resetGame();
   }
 }
 
-// ============================
 // Game Logic
-// ============================
 
 function nextPhase() {
-  if (gamePhase === 1) {
-    gamePhase = 2;
-    spawnBalls(2);
-  } else if (gamePhase === 2) {
-    gamePhase = 3;
-    spawnBalls(3);
-  } else if (gamePhase === 3) {
-    if (lastDropTimes.length >= 3) {
+  if (gamePhase===1) { gamePhase=2; spawnBalls(2); }
+  else if (gamePhase===2) { gamePhase=3; spawnBalls(3); }
+  else if (gamePhase===3) {
+    if (lastDropTimes.length>=3) {
       const last3 = lastDropTimes.slice(-3);
-      if (last3[2] - last3[0] < 1000) {
-        startRainbowMode();
-        return;
-      }
+      if (last3[2]-last3[0]<1000) { startRainbowMode(); return; }
     }
     spawnBalls(3);
   }
@@ -105,75 +119,71 @@ function nextPhase() {
 
 function spawnBalls(n) {
   balls = [];
-  for (let i = 0; i < n; i++) {
-    const x = bar.x + random(-bar.len / 2.5, bar.len / 2.5);
+  for (let i=0; i<n; i++) {
+    const x = bar.x + random(-bar.len/2.5, bar.len/2.5);
     const y = bar.y - 100;
-    const col = color(ballColors[i % ballColors.length]);
-    balls.push(new Ball(x, y, col));
+    const col = color(ballColors[i%ballColors.length]);
+    balls.push(new Ball(x,y,col));
   }
+}
+
+function activateTopFlippers() {
+  topFlippersActive = true;
+  let topY = height - 180;
+  topLeftFlipper = new Flipper(200, topY, 140, "left");
+  topRightFlipper = new Flipper(width-200, topY, 140, "right");
 }
 
 function startRainbowMode() {
   gamePhase = "rainbow";
-  balls = [];
-  waves = [];
-  rainbowWave = new RainbowFillWave();
+  balls=[]; waves=[]; rainbowWave = new RainbowFillWave();
   rainbowStartTime = millis();
 }
 
 function resetGame() {
-  gamePhase = 1;
-  balls = [];
-  waves = [];
-  rainbowWave = null;
-  lastDropTimes = [];
-  colorShift = 0;
-  bar = new Bar(width / 2, height / 2, 360);
+  gamePhase = 1; topFlippersActive=false; balls=[]; waves=[]; rainbowWave=null; lastDropTimes=[];
+  colorShift=0; bar = new Bar(width/2,height/2,360);
   spawnBalls(1);
+
+  let bottomY = height-70;
+  leftFlipper = new Flipper(140,bottomY,160,"left");
+  rightFlipper = new Flipper(width-140,bottomY,160,"right");
 }
 
-// ============================
 // Mouse Control
-// ============================
 
-function mousePressed() { dragging = true; }
-function mouseReleased() { dragging = false; }
+function mousePressed() { dragging=true; }
+function mouseReleased() { dragging=false; }
 
 function mouseDragged() {
   if (gamePhase !== "rainbow" && dragging) {
-    bar.angle += (pmouseY - mouseY) * 0.004;
-    bar.angle = constrain(bar.angle, -PI / 4, PI / 4);
+    bar.angle += (pmouseY - mouseY)*0.004;
+    bar.angle = constrain(bar.angle, -PI/4, PI/4);
   }
-  if (gamePhase === "rainbow") {
-    // Made colorShift effect more subtle in the new animation
-    colorShift += (mouseY - pmouseY) * 0.005;
-  }
+  if (gamePhase === "rainbow") colorShift += (mouseY - pmouseY)*0.005;
 }
 
-// ============================
-// Handle Ball Collisions
-// ============================
+// Ball–Ball Collisions
 
 function handleCollisions() {
-  for (let i = 0; i < balls.length; i++) {
-    for (let j = i + 1; j < balls.length; j++) {
-      const b1 = balls[i];
-      const b2 = balls[j];
-      const distBetween = p5.Vector.dist(b1.pos, b2.pos);
-      const minDist = (b1.r + b2.r) / 2;
+  for (let i=0; i<balls.length; i++) {
+    for (let j=i+1; j<balls.length; j++) {
+      const b1=balls[i], b2=balls[j];
+      const distBetween = p5.Vector.dist(b1.pos,b2.pos);
+      const minDist = (b1.r+b2.r)/2;
 
-      if (distBetween < minDist) {
-        const overlap = (minDist - distBetween) / 2;
-        const dir = p5.Vector.sub(b1.pos, b2.pos).normalize();
+      if (distBetween<minDist) {
+        const overlap = (minDist-distBetween)/2;
+        const dir = p5.Vector.sub(b1.pos,b2.pos).normalize();
         b1.pos.add(dir.copy().mult(overlap));
         b2.pos.sub(dir.copy().mult(overlap));
 
-        const relVel = p5.Vector.sub(b1.vel, b2.vel);
+        const relVel = p5.Vector.sub(b1.vel,b2.vel);
         const velAlongNormal = relVel.dot(dir);
-        if (velAlongNormal > 0) continue;
+        if (velAlongNormal>0) continue;
 
         const restitution = 0.8;
-        const impulse = -(1 + restitution) * velAlongNormal / 2;
+        const impulse = -(1+restitution)*velAlongNormal/2;
         const impulseVec = dir.copy().mult(impulse);
 
         b1.vel.add(impulseVec);
